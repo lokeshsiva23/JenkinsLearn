fe4b428e9e4776065b1975c4854fd165f0ad063e
lokeshsiva23 <lokeshsiva23@gmail.com>
2025-05-21 07:13:34 +0530

    Update SampleFile.java

--- SampleFile.java
+++ SampleFile.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Siva!");
         System.out.println("Hello, Lokesh!");
         System.out.println("Hello, Loke!");
     }
